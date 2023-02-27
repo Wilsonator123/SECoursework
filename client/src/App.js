21a3f40ff48abc3037e4f307b1eb38e1df6057bb
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-02-27 13:54:01 +0000

    Setup
    Set up server and dB

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
-
-import './App.css';
+import "./App.css";
+import { useState, useEffect, Fragment, useRef } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 function App() {
-  return (
-    <div> 
-      
-    </div>
-      
-  );
+    return <div></div>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-const app = require("express");
+const express = require("express");
 const path = require("path");
-const sql = require("better-sqlite3");
 const bodyParser = require("body-parser");
-
 const app = express();
+const dB = require("better-sqlite3");
+const fs = require("fs");
 
-app.get("/", (req))
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+const database = new dB("database.db", { verbose: console.log });
+
+database.exec(fs.readFileSync(path.join(__dirname, "ddl.sql"), "utf8"));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
