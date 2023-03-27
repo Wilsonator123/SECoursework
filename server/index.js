4a031ac312a3dc8e8c8320f1ffba2e91e0c447e7
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-03-27 15:17:18 +0100

    added interface

--- server/index.js
+++ server/index.js
@@ -3,24 +3,10 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const dB = require("better-sqlite3");
 const fs = require("fs");
 const cors = require("cors");
 const multer = require("multer");
-const { checkEmailFormat } = require("./interface.js");
 
-/*************************MIDDLEWARE******************************/
-
-app.use(cors({ origin: true, credentials: true }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-app.use(bodyParser.json());
-
-/*************************DATABASE TINGS******************************/
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "../client/public/images/");
@@ -31,7 +17,23 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-const database = new dB("database.db", { verbose: console.log });
+
+const { Interface } = require("./interface.js");
+
+/*************************MIDDLEWARE******************************/
+
+const interface = new Interface();
+app.use(cors({ origin: true, credentials: true }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+app.use(bodyParser.json());
+
+/*************************DATABASE TINGS******************************/
+
 /**
  * This will handle all requests from the server
  */
@@ -51,100 +53,37 @@ app.get("/", (req, res) => {
 /*************************LOG IN PAGE******************************/
 
 app.post("/api/checkEmail", (req, res) => {
-    if (!checkEmailFormat(req.body.email)) res.send(false);
-    const stmt = database.prepare("SELECT * FROM user WHERE email = ?");
-    const info = stmt.all(req.body.email);
-    info.length === 0 ? res.send(true) : res.send(false);
+    res.send(interface.checkEmailFormat(req.body.email));
 });
 
 app.post("/api/checkUsername", (req, res) => {
-    const stmt = database.prepare("SELECT * FROM user WHERE username = ?");
-    const info = stmt.all(req.body.username);
-    info.length === 0 ? res.send(true) : res.send(false);
+    res.send(interface.checkUsername(req.body.username));
 });
 
-// app.get("/api/checkPassword", (req, res) => {
-//     const stmt = database.prepare("SELECT * FROM user WHERE password = '$1'");
-//     const info = stmt.all(req.body.password);
-//     info.rows() > 0 ? res.send(false) : res.send(true);
-// });
+app.get("/api/checkPassword", (req, res) => {
+    if (req.body.password === req.body.dpassword) res.send(true);
+    else res.send(false);
+});
 
 app.post("/api/login", (req, res) => {
-    const stmt = database.prepare(
-        "SELECT * FROM user WHERE username = ? AND password = ?"
-    );
-    const info = stmt.all(req.body.username, req.body.password);
-    if (info.length === 0) res.send(false);
-    else res.send(true);
+    res.send(interface.checkLogin(req.body.login, req.body.password));
 });
 
-app.post("/api/createUser", (req, res) => {
-    const {
-        username,
-        firstname,
-        lastname,
-        gender,
-        password,
-        email,
-        weight,
-        height,
-        tweight,
-        dob,
-    } = req.body;
-    if (
-        username === "" ||
-        firstname === "" ||
-        lastname === "" ||
-        gender === "" ||
-        password === "" ||
-        email === "" ||
-        weight === "" ||
-        height === "" ||
-        tweight === "" ||
-        dob === ""
-    ) {
-        res.send(false);
-        return;
-    }
-
-    if (req.file === undefined) img = "default.png";
-    else img = req.body.username + "pp.png";
-    const stmt = database.prepare(
-        "INSERT INTO user (username, firstname, lastname, gender, password, email, weight, height, tweight, dob, img) VALUES (?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?)"
-    );
-    const info = stmt.run(
-        username,
-        firstname,
-        lastname,
-        gender,
-        password,
-        email,
-        weight,
-        height,
-        tweight,
-        dob,
-        img
-    );
-    if (info.changes === 1) res.send(true);
-    else res.send(false);
+app.post("/api/createUser", upload.single("file"), (req, res) => {
+    var img;
+    if (body.file === undefined) img = "default.png";
+    else img = body.username + "pp.png";
+    res.send(interface.createUser(req.body, img));
 });
 
 /*************************HOME PAGE******************************/
 
 app.get("/api/getActiveGoals", (req, res) => {
-    const stmt = database.prepare(
-        "SELECT * FROM goals WHERE id = '$1' AND status NOT IN ('inactive')"
-    );
-    const info = stmt.all(req.id);
-    res.send(info);
+    res.send(interface.getActiveGoals(req.body.id));
 });
 
 app.get("/api/getGoalHistory", (req, res) => {
-    const stmt = database.prepare(
-        "SELECT * FROM goals WHERE id = '$1' AND status IN ('inactive')"
-    );
-    const info = stmt.all(req.id);
-    res.send(info);
+    res.send(interface.getGoalHistory(req.body.id));
 });
 
 app.listen(3001, () => {
