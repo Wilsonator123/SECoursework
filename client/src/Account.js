597ebc90ba80eb8d4cab3d3c89bdbfc21e4bdbba
driverboy101 <123957179+driverboy404@users.noreply.github.com>
2023-04-04 14:32:17 +0100

    may have now fixed Account.js

--- client/src/Account.js
+++ client/src/Account.js
@@ -1,6 +1,5 @@
 
 import HealthDetails from "./components/HealthDetails";
-import MealHistory from "./components/MealHistory";
 
 import React, { useState, useEffect } from "react";
 
@@ -16,11 +15,21 @@ export default function Account({ userID }) {
     const userToken = JSON.parse(tokenString);
 
 
-    //Fetch a list of all the exercises
-    useEffect(() => {
+    const [userMeals, setUserMeals] = useState([]);
+
+    //Used to get the date for which we want to view exercises and meals
+    const [date, setDate] = useState(new Date());
+
+
+
+
+
+
+
+    const getExercise = (date) => {
         fetch("http://localhost:3001/api/getUserExercises", {
             method: "POST",
-            body: JSON.stringify({userToken}),
+            body: JSON.stringify({id: userToken, date: date.toLocaleDateString("en-GB")}),
             headers: {
                 "Content-Type": "application/json",
             },
@@ -43,11 +52,61 @@ export default function Account({ userID }) {
                 console.error("Error:", error);
             })
             
+        }
+
+
+        const getMeals = (date) => {
+            fetch("http://localhost:3001/api/getUserMeals", {
+                method: "POST",
+                body: JSON.stringify({id: userToken, date: date.toLocaleDateString("en-GB")}),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+    
+            })
+    
+            .then((response) => response.json())
+    
+            .then((data) => {
+    
+            if (data) {
+                console.log(data);
+                setUserMeals(data); 
+                } else {
+                        alert("Could not find meals for this user.")
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                })
+                
+            }
+
+    
+
+
+    useEffect(() => {
+        getExercise(date);
+        getMeals(date);    
         }, []);
 
 
-
-
+        //Change the date for which we get exercises and meals when
+        //arrow buttons are pressed
+        //Keep it in the lovely DD-MM-YYYY format
+        const goToPrevDay = () => {
+            const prevDay = new Date(date.getTime() - (24 * 60 * 60 * 1000));
+            setDate(prevDay);
+            getExercise(prevDay);
+            getMeals(prevDay);
+          };
+        
+        const goToNextDay = () => {
+            const nextDay = new Date(date.getTime() + (24 * 60 * 60 * 1000));
+            setDate(nextDay);
+            getExercise(nextDay);
+            getMeals(nextDay);
+        };
 
 
 
@@ -67,7 +126,13 @@ export default function Account({ userID }) {
             <br/><br/>
 
 
+            <div>
+            <button onClick={goToPrevDay}> Previous </button>
+                <p>Date: {date.toLocaleDateString("en-GB")}</p>    
+            <button onClick={goToNextDay}> Next </button>
+            </div>
 
+            <br/><br/>
 
 
             <div>
@@ -87,13 +152,31 @@ export default function Account({ userID }) {
 
 
             <br/><br/>
-
             <div>
-                <h2>Meal History</h2>
-                {/*<MealHistory />*/}
+                <h2>Meal History: </h2>
+                
+                {userMeals.map(userMeal=> (
+                    <div key={userMeal.id}>
+                        <br/>
+                        <p style={{display: 'inline'}}>{userMeal.mealType}: {userMeal.name}</p>
+                        <br/>
+                        <p style={{display: 'inline'}}>{userMeal.food_name}: {userMeal.foodAmount / 100 * userMeal.food_calories} kcals</p>
+                        <br/>
+                        <p style={{display: 'inline'}}>{userMeal.drink_name}: {userMeal.drinkAmount / 100 * userMeal.drink_calories} kcals</p>
+                        <br/>
+                    </div>
 
-            </div>   
+                    
+                     ))}
 
+
+                    {userMeals.reduce((total, userMeal) => total + ((userMeal.foodAmount / 100 * userMeal.food_calories) + (userMeal.drinkAmount / 100 * userMeal.drink_calories)), "") && 
+                        <div> 
+                            <br/>
+                            <p>Total: {userMeals.reduce((total, userMeal) => total + ((userMeal.foodAmount / 100 * userMeal.food_calories) + (userMeal.drinkAmount / 100 * userMeal.drink_calories)), 0)} kcals</p> 
+                        </div>    }
+
+        </div>
         </div>
     );
 }
