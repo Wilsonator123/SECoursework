611ce54be4a10b70b9fc78c9d562fc218779f923
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-04-10 11:23:16 +0100

    working

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
                     </li>
 
                     <li>
-                        <Link to="/Meal">Record Meal</Link>
+                        <Link to="/Diet">Record Meal</Link>
                     </li>
 
                     <li>
