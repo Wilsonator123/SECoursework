e02dfaeb88263509888cea60df837dff45e4e271
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-04-19 16:16:54 +0100

    Update Goal.js

--- client/src/Goal.js
+++ client/src/Goal.js
@@ -1,19 +1,93 @@
+@@ -1,19 +1,91 @@
+import React, { useState } from "react";
 import "./css/goal.css";
 
 
 export default function Home() {
-    return (
-        <div id="pageContainer">
-            <h1> GOAL PAGE </h1>
-            <div class="grid-container-goal">
-            <div class="goalBox1">
-                <h2>Diet</h2>
-            </div>
-            <div class="goalBox2">
-                <h2>Exercise</h2>
-            </div>
-            </div>
+  const [showExerciseGoalForm, setShowExerciseGoalForm] = useState(false);
+  const [showDietGoalForm, setShowDietGoalForm] = useState(false);
+
+  const handleExerciseGoalClick = () => {
+    setShowExerciseGoalForm((prev) => !prev);
+  };
+
+  const handleDietGoalClick = () => {
+    setShowDietGoalForm((prev) => !prev);
+  };
+
+  const handleExerciseFormSubmit = (event) => {
+    event.preventDefault();
+    // Handle exercise goal form submission
+  };
+
+  const handleDietFormSubmit = (event) => {
+    event.preventDefault();
+    // Handle diet goal form submission
+  };
+
+  return (
+    <div id="pageContainer">
+      <h1> GOAL PAGE </h1>
+      <div className="grid-container-goal">
+        <div className="goalBox1">
+          <h2>Diet</h2>
+          <button onClick={handleDietGoalClick}>
+            {showDietGoalForm ? "-" : "+"}
+          </button>
+          {showDietGoalForm && (
+            <form onSubmit={handleDietFormSubmit}>
+              <label>
+                Goal Name:
+                <input type="text" />
+              </label>
+              <label>
+                Date Begun:
+                <input type="date" />
+              </label>
+              <label>
+                Date Due:
+                <input type="date" />
+              </label>
+              <label>
+                Weight:
+                <input type="number" />
+              </label>
+              <button type="submit">Submit goal</button>
+            </form>
+          )}
         </div>
-        
-    );
+        <div className="goalBox2">
+          <h2>Exercise</h2>
+          <button onClick={handleExerciseGoalClick}>
+            {showExerciseGoalForm ? "-" : "+"}
+          </button>
+          {showExerciseGoalForm && (
+            <form onSubmit={handleExerciseFormSubmit}>
+              <label>
+                Goal Name:
+                <input type="text" />
+              </label>
+              <label>
+                Date Begun:
+                <input type="date" />
+              </label>
+              <label>
+                Date Due:
+                <input type="date" />
+              </label>
+              <label>
+                Time:
+                <input type="number" />
+              </label>
+              <label>
+                Distance:
+                <input type="number" />
+              </label>
+              <button type="submit">Submit goal</button>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
