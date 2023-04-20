b9862393d3512db80b3fc8eda2c76f39e75add66
Chanelmorgan <chanelmorgan05@icloud.com>
2023-04-20 10:50:08 +0100

    Nav bar
    changed the Nav bar to be responsive
    
    Co-Authored-By: Grace Johnson <106590642+gracetoxicstar02@users.noreply.github.com>

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./Navbar";
 import { Outlet, Link } from "react-router-dom";
 import "./css/header.css";
 
-export default function Navbar() {
+export default function Navbar() { 
+    useEffect(() => {
+        const hamburger = document.querySelector(".hamburger");
+        const nav = document.querySelector("nav");
+    
+        hamburger.addEventListener("click", function () {
+          nav.classList.toggle("active");
+        });
+      }, []);
+
+
     return (
         <>
-            TEST
-            <nav>
+            {/* <nav>
                 <ul>
                     <li>
                         <Link exac to="/">
                             LOGO
-                        </Link>
+                        </Link> 
                     </li>
 
                     <li>
@@ -35,7 +44,43 @@ export default function Navbar() {
                         <Link to="/Account">Account</Link>
                     </li>
                 </ul>
+            </nav> */}
+            <header>
+        <div class="header-left">
+            <div class="logo">
+                {/* <img src="./logo.png" alt=""> */}
+                Logo
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                    <Link to="/Meal">Record Meal</Link>
+                    </li>
+                    <li>
+                    <Link to="/Exercise">Record Exercise</Link>
+                    </li>
+                    <li>
+                    <Link to="/Goal">Goals</Link>
+                    </li>
+                    <li>
+                    <Link to="/Group">Groups</Link>
+                    </li>
+                    <li>
+                    <Link to="/Account">Account</Link>
+                    </li>
+                </ul>
+                
             </nav>
+        </div>
+        <div class="header-right">
+            <div class="hamburger">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </header>
+            
             <Outlet />
         </>
     );
